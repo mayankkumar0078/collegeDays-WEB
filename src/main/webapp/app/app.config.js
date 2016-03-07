3cf8d439d9dcc426b39840e3e0894267c014b7d5
MAYANK KUMAR <mayankkumar0078@gmail.com>
2016-03-07 13:57:34 +0530

    Integration of UserService with UI

--- src/main/webapp/app/app.config.js
+++ src/main/webapp/app/app.config.js
@@ -1,42 +1,34 @@
-// Declare app level module which depends on controllers, services, directives and filters module.
-var module = angular.module('collegeDays',['home','studentDashboard', 'ui.router','blockUI',
-                                           'ui.bootstrap','ngResource','awesome-rating','ngDialog','infinite-scroll'
-                                           ,'nsPopover']);
+var module = angular.module('collegeDays', [ 'home', 'studentDashboard',
+		'ui.router', 'blockUI', 'ui.bootstrap', 'ngResource', 'awesome-rating',
+		'ngDialog', 'infinite-scroll', 'nsPopover' ]);
 
-
-module.config(function(blockUIConfig,$stateProvider, $urlRouterProvider){
+module.config(function(blockUIConfig, $stateProvider, $urlRouterProvider) {
 	blockUIConfig.message = 'Please wait.. !';
 	blockUIConfig.autoBlock = false;
 	$urlRouterProvider.otherwise('/home');
-	//$stateProvider.state('home',{url:'/home', templateUrl:'app/home/home.tpl.html', controller:'HomeCtrl'});
 });
 
+module.controller('appCTRL', function appCTRL($scope, $location, $rootScope,
+		ngDialog, TokenStorage) {
 
-module.controller('appCTRL', function appCTRL($scope, $location, $rootScope, ngDialog) {
-	
 	$rootScope.userLoggedIn = false;
 	$rootScope.user = null;
-	$scope.redirectToHomePage = function(){
+	$scope.redirectToHomePage = function() {
 		$location.path("/home");
 	};
-	
-	$scope.openLoginDialog = function(){
-		//open the login modal
-		ngDialog.open({template: 'loginDialogContent.html',
-					   controller: 'loginDialogCtrl',
-					   scope: $scope});
-	};
 
-	//logout the user
-	//logout the user
-	$scope.logOut = function(){
+	$scope.openLoginDialog = function() {
+		ngDialog.open({
+			template : 'loginDialogContent.html',
+			controller : 'loginDialogCtrl as loginCtrl',
+			scope : $scope
+		});
+	};
+	$scope.logOut = function() {
 		$rootScope.userLoggedIn = false;
 		$rootScope.user = null;
-		//redirect the user to home page
+		TokenStorage.clear();
 		$scope.redirectToHomePage();
 	};
-	
+
 });
-
-
-
--- src/main/webapp/app/login/login.controller.js
+++ src/main/webapp/app/login/login.controller.js
@@ -1,80 +1,109 @@
 var homeModule1 = angular.module('home');
-homeModule1.controller('loginDialogCtrl', function ($scope, $rootScope, $location, loginService) {
-	
-	initialiseModels();
+homeModule1.factory('TokenStorage', function() {
+	var storageKey = 'auth_token';
+	return {		
+		store : function(token) {
+			return localStorage.setItem(storageKey, token);
+		},
+		retrieve : function() {
+			return localStorage.getItem(storageKey);
+		},
+		clear : function() {
+			return localStorage.removeItem(storageKey);
+		}
+	};
+}).factory('TokenAuthInterceptor', function($q, TokenStorage) {
+	return {
+		request: function(config) {
+			var authToken = TokenStorage.retrieve();
+			if (authToken) {
+				config.headers['X-AUTH-TOKEN'] = authToken;
+			}
+			return config;
+		},
+		responseError: function(error) {
+			if (error.status === 401 || error.status === 403) {
+				TokenStorage.clear();
+			}
+			return $q.reject(error);
+		}
+	};
+}).config(function($httpProvider) {
+	$httpProvider.interceptors.push('TokenAuthInterceptor');
+});
+
+
+homeModule1.controller('loginDialogCtrl', function($scope, $rootScope,
+		$location, loginService, $http,TokenStorage) {
+
 	//make the login home page visible when the login page is landed
 	$scope.loginHomePage = true;
-	
-	$scope.showSignInForm = function(){
+
+	//declare the model for forgot password
+	$scope.forgotPasswordEmail = "";
+
+	$scope.showSignInForm = function() {
 		$scope.signInForm = true;
 		$scope.signUpForm = false;
 		$scope.forgotPasswordForm = false;
 		$scope.loginHomePage = false;
+		$scope.isLoginFailed=false;
 	};
-	
-	$scope.showSignUpForm = function(){
+
+	$scope.showSignUpForm = function() {
 		$scope.signInForm = false;
 		$scope.signUpForm = true;
 		$scope.forgotPasswordForm = false;
 		$scope.loginHomePage = false;
 	};
-	
-	$scope.showForgotPasswordForm = function(){
+
+	$scope.showForgotPasswordForm = function() {
 		$scope.signInForm = false;
 		$scope.signUpForm = false;
 		$scope.forgotPasswordForm = true;
 		$scope.loginHomePage = false;
 	};
 
-	function initialiseModels(){
-		//declare model variables for sign in
-		$scope.signInEmail = "";
-		$scope.signInPpassword="";
-		//declare model variables for sign up
-		$scope.signUpName = "";
-		$scope.signUpEmail = "";
-		$scope.signUpPassword = "";
-		$scope.signUpCollege = "";
-		//declare the model for forgot password
-		$scope.forgotPasswordEmail="";
-	}
-	$scope.verifyLogin = function(){
-		
-		var loginCredential = {
-									email : $scope.email,
-									password : $scope.password
-							   };
-		//call the login service to verify the login credentials
-		var resource = loginService.verifyLogin();
-		//make the ajax call to verify the login
-	/*	resource.call(loginCredential).$promise.then(function(response) {
-			
-		},
-		function(error) {
-			alert(error);
-			blockUI.stop();
-		}
-		);*/
-		
-		//check if the user is authenticated
-		if(true){
-			$location.path("/studentDashboard/library");
-			$scope.closeThisDialog('Some value');
-			//set the rootScope variable 
-			$rootScope.userLoggedIn = true;
-			//set the user coming from the response
-			$rootScope.user = {};
-		}
-		
-	};
 	
+	var self=this; 
 	/*registers a new user*/
-	$scope.registerUser = function(){
+	self.registerUser = function() {
+		$http.post("http://localhost:9090/userModule/Users", self.user)
+				.success(function(data, status, headers, config) {	
+						
+				}).error(function(data, status, header, config) {
+					alert("Error");
+					console.log("Error");
+				});
+
 	};
-	
-	/*retrieves the password for the email*/
-	$scope.recoverPassword = function(){
-		
+
+	self.login = function() {
+
+		$http.post('http://localhost:9090/userModule/Users/authenticate',
+				self.loginCredentials).success(
+				function(result, status, headers) {
+					console.log(result.token);
+					self.authenticated = true;
+					TokenStorage.store(result.token);
+					if (self.authenticated) {
+						$location.path("/studentDashboard/library");
+						self.closeThisDialog('Some value');
+						// set the rootScope variable
+						$rootScope.userLoggedIn = true;
+						// set the user coming from the response
+						$rootScope.user = result.firstName;
+					}
+
+				}).error(function(result, status, headers) {
+			self.isLoginFailed = true;
+			self.errorMessage = result.message;
+			console.log(self.errorMessage);
+		});
 	};
-	
+	/* retrieves the password for the email */
+	$scope.recoverPassword = function() {
+
+	};
+
 });
